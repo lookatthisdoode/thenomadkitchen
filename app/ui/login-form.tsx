eae89ddad5c7a10496d548956b657512fc4ea0fe
lookatthisdoode <radchenko.andreii@gmail.com>
2024-07-02 19:08:09 +0200

    fixing bug with searchParams()

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/app/ui/button";
 import { useFormStatus } from "react-dom";
 import { authenticate } from "../lib/actions";
 import { useRouter, useSearchParams } from "next/navigation";
-import { FormEvent, useState } from "react";
+import { FormEvent, Suspense, useState } from "react";
 
 export default function LoginForm() {
   const newUrl = useSearchParams().get("callbackUrl");
@@ -34,65 +34,67 @@ export default function LoginForm() {
     <div
       className={`bg-background w-full h-screen flex justify-center items-center`}
     >
-      <form onSubmit={(e) => handleSubmit(e)} className="space-y-3">
-        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-          <h1 className={` mb-3 text-2xl`}>Please log in to continue.</h1>
-          <div className="w-full">
-            <div>
-              <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <div className="relative">
-                <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] px-2 text-sm outline-2 placeholder:text-gray-500"
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="Enter your email address"
-                  required
-                />
-                {/*<AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />*/}
+      <Suspense fallback={null}>
+        <form onSubmit={(e) => handleSubmit(e)} className="space-y-3">
+          <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+            <h1 className={` mb-3 text-2xl`}>Please log in to continue.</h1>
+            <div className="w-full">
+              <div>
+                <label
+                  className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] px-2 text-sm outline-2 placeholder:text-gray-500"
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email address"
+                    required
+                  />
+                  {/*<AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />*/}
+                </div>
+              </div>
+              <div className="mt-4">
+                <label
+                  className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <div className="relative">
+                  <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] px-2 text-sm outline-2 placeholder:text-gray-500"
+                    id="password"
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    required
+                    minLength={6}
+                  />
+                  {/*<KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />*/}
+                </div>
               </div>
             </div>
-            <div className="mt-4">
-              <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <div className="relative">
-                <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] px-2 text-sm outline-2 placeholder:text-gray-500"
-                  id="password"
-                  type="password"
-                  name="password"
-                  placeholder="Enter password"
-                  required
-                  minLength={6}
-                />
-                {/*<KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />*/}
-              </div>
+            <LoginButton />
+            <div
+              className="flex h-8 items-end space-x-1"
+              aria-live="polite"
+              aria-atomic="true"
+            >
+              {errorMessage && (
+                <>
+                  {/*<ExclamationCircleIcon className="h-5 w-5 text-red-500" />*/}
+                  <p className="text-sm text-red-500">{errorMessage}</p>
+                </>
+              )}
             </div>
           </div>
-          <LoginButton />
-          <div
-            className="flex h-8 items-end space-x-1"
-            aria-live="polite"
-            aria-atomic="true"
-          >
-            {errorMessage && (
-              <>
-                {/*<ExclamationCircleIcon className="h-5 w-5 text-red-500" />*/}
-                <p className="text-sm text-red-500">{errorMessage}</p>
-              </>
-            )}
-          </div>
-        </div>
-      </form>
+        </form>
+      </Suspense>
     </div>
   );
 }
